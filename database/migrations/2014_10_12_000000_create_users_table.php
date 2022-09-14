c050d85d281e0a5ea210494d19d7f7f505ece5ac
Davematics <davgwuche@gmail.com>
2022-09-14 09:48:33 +0100

    first commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->bigInteger('phone_number')->unique();
+            $table->string('phone_number')->unique();
             $table->string('country');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
